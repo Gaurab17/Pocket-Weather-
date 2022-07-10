9ee5051d750746677baa4442813cfef2af044f7d
Gaurab17 <gaurabstha001@gmail.com>
2022-07-10 23:46:05 +0545

    First web commit

---
+++ js/forecast.js
@@ -0,0 +1,68 @@
+const key = 'zFqUAuWFvOVenf5YjLP5GM1nxSh4GOTI';
+const searchCity = document.querySelector('.search');
+const cityName = document.querySelector('.city_name');
+const weatherCondition = document.querySelector('.weather_condition');
+const temperature = document.querySelector('.temperature');
+const image = document.querySelector('img');
+const result = document.querySelector('.result');
+
+//get city search
+const getCity = async (city) =>{
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base+query);
+    const cityData = await response.json();
+    return cityData[0];
+}
+
+//get city Weather conditions
+const getWeather = async (id) =>{
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query =  `${id}?apikey=${key}`;
+    const response = await fetch(base+query);
+    const data = await response.json();
+    return data[0];
+}
+
+//search the city
+const search = async (d) =>{
+    const cityDetails = await getCity(d);
+    const weatherDetails = await getWeather(cityDetails.Key);
+    cityName.innerHTML = `<h4>${cityDetails.LocalizedName}</h4>`
+    weatherCondition.innerHTML = `<h4>${weatherDetails.WeatherText}</h4>`
+    temperature.innerHTML = `<h2>${weatherDetails.Temperature.Metric.Value} °C</h2>`
+    
+    return {
+        cityDetails:cityDetails,
+        weatherDetails:weatherDetails
+    }
+}
+
+//update uUI
+const updateUI = () =>{
+    search(localStorage.getItem('city'))
+        .then(data=>{
+            let timeSrc = null;
+            if(data.weatherDetails.IsDayTime){
+                timeSrc = "img/day.png";
+            } else{
+                timeSrc = "img/night.png";
+            } 
+            image.setAttribute('src', timeSrc);
+        })
+        .catch(err=>console.log(err));
+    result.style.display = "block";
+}
+
+searchCity.addEventListener('submit', e=>{
+    e.preventDefault();
+    const city = searchCity.search.value.trim();
+    localStorage.setItem('city', city);
+    searchCity.reset();
+    let timeSrc = null;
+    updateUI();
+});
+
+if(localStorage.getItem('city')){
+    updateUI();
+}
